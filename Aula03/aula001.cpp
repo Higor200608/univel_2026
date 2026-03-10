403ff67d46aa5ed7f874e12511e34d1a00b15db0
Higor C. de Oliveira <higorcavalheiro2682006@gmail.com>
2026-03-10 20:37:30 -0300

    Create aula001.cpp

---
+++ Aula03/aula001.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    float num1, num2;
+
+    cout<<"Hello World";
+
+    cout<<"Digite o primeiro numero: "; //print 
+
+    cin >> num1; // input
+
+    cout<<"Digite o segundo numero: ";
+
+    cin >> num2;
+
+    cout<<(num1 + num2) << " foda" << endl;
+    
+    cout<< num1 << endl; // endl é quebra de linha
+    cout<< num2 << endl;
+
+    return 0;
+}
